a58c5cae4e555b0dd5d9be39557bd798f21d6c50
Shuvonkor Pramanik <shuvonkorpramanik25@gmail.com>
2023-05-17 00:06:23 +0600

    navbar design update

--- js/script.js
+++ js/script.js
@@ -1,17 +1,17 @@
-const deliveryMan = document.getElementById("delivery-man");
-const deliveryManImg = deliveryMan.querySelector("img");
+// const deliveryMan = document.getElementById("delivery-man");
+// const deliveryManImg = deliveryMan.querySelector("img");
 
-let currentPosition = 0;
+// let currentPosition = 0;
 
-function animateDeliveryMan() {
-  currentPosition += 2;
-  deliveryManImg.style.left = currentPosition + "px";
+// function animateDeliveryMan() {
+//   currentPosition += 2;
+//   deliveryManImg.style.left = currentPosition + "px";
 
-  if (currentPosition >= +200) {
-    currentPosition = 0;
-  }
+//   if (currentPosition >= +200) {
+//     currentPosition = 0;
+//   }
 
-  window.requestAnimationFrame(animateDeliveryMan);
-}
+//   window.requestAnimationFrame(animateDeliveryMan);
+// }
 
-animateDeliveryMan();
+// animateDeliveryMan();
